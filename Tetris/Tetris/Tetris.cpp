db60ff72ae962724849b633ae29032328e6f3c5d
AmosLeCoq <79831394+AmosLeCoq@users.noreply.github.com>
2022-01-12 16:33:59 +0100

    Add files via upload

---
+++ Tetris/Tetris/Tetris.cpp
@@ -0,0 +1,78 @@
+// Tetris.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
+//
+
+#include <iostream>
+#include <fcntl.h>
+#include <stdlib.h> 
+
+using namespace std;
+
+int tab1[15][10];
+
+int x = 10;
+int y = 15;
+
+void reset(int op) {
+
+	switch (op)
+	{
+	case 1:
+		break;
+	case 2:
+		break;
+
+	default:
+
+		break;
+	}
+
+	for (int a = 0; a < y; a++) {
+		for (int b = 0; b < x; b++) {
+			tab1[a][b] = 0;
+		}
+	}
+
+}
+
+void jeu() {
+
+	int ra = rand() % 7;
+
+	switch (ra)
+	{
+	case 1:
+		break;
+
+	case 2:
+		break;
+
+	case 3:
+		break;
+
+	case 4:
+		break;
+
+	case 5:
+		break;
+
+	case 6:
+		break;
+
+	case 7:
+		break;
+
+	default:
+
+		break;
+	}
+}
+
+int main()
+{
+	reset(1);
+
+	reset(2);
+} 
+
+
+
